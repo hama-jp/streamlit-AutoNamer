57c63b4362c91d6033321a6ef50b0a13a080666c
hama-jp <52596960+hama-jp@users.noreply.github.com>
2023-03-16 16:03:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ st.title("AutoNamer")
 st.header('変数名や関数名を自動で命名するアプリです')
 selected_language = st.selectbox("言語を選択してください", language_list)
 text_input = st.text_input("変数や関数にする処理の概要を記述してください", "")
-openai.api_key = st.text_input("ChatGPT API Keyを記載してください。","")
 if st.button("生成"):
     names = generate_name(text_input, language=selected_language)
     for name in names.split(","):

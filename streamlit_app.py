1dc101469ebe936182e13c5f05b875c1951fc61b
hama-jp <52596960+hama-jp@users.noreply.github.com>
2023-03-16 15:15:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai
+import os
+//CHATGPT_API_KEY = os.environ["CHATGPT_API_KEY"]
+//openai.api_key = CHATGPT_API_KEY
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def generate_name(text,language):
+    res = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": f"Please output 10 candidates for appropriate variable or function names in English from the input Japanese. Naming conventions must follow the {language} language specification and the output must be on a single line in the format name1,name2,name3."
+        },
+        {
+            "role": "user",
+            "content": text
+        },
+    ],
+    )
+    return res["choices"][0]["message"]["content"]
+language_list = ["Python", "JavaScript", "Java", "Go"]
+st.title("AutoNamer")
+st.header('変数名や関数名を自動で命名するアプリです')
+selected_language = st.selectbox("言語を選択してください", language_list)
+text_input = st.text_input("変数や関数にする処理の概要を記述してください", "")
+openai.api_key = st.text_input("ChatGPT API Keyを記載してください。","")
+if st.button("生成"):
+    names = generate_name(text_input, language=selected_language)
+    for name in names.split(","):
+        st.code(name)
